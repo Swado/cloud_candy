315b6cb9738ac1926dbddcf06817a3aa06178295
Swaraj <swarajbdolhare@gmail.com>
2023-10-14 11:42:04 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.cloud_candy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:cloud_candy/app/app.dart';
+import 'package:cloud_candy/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
